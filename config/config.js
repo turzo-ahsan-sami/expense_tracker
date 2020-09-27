bf31f22580f36f83e543350b9e10483ebf088860
turzo267725 <turzo.dev@gmail.com>
2020-09-27 10:29:09 +0600

    mongo cloud cluster creds added

--- config/config.js
+++ config/config.js
@@ -1,12 +1,8 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
+  mongoUri: process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ml2ox.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`
 }
 
 export default config
